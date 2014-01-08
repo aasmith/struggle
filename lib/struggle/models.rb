7aff16a4e7e70d93c8e2675b68871f2655b7fa12
Andrew A Smith <andy@tinnedfruit.org>
2014-01-08 15:41:46 -0800

    Ensure instruction lists are well-formed.

--- lib/struggle/models.rb
+++ lib/struggle/models.rb
@@ -106,6 +106,11 @@ module Instructions
 
   class NestingInstruction < Instruction
     def initialize(*instructions)
+      unless instructions.all? { |i| WorkItem === i }
+        bad = instructions.reject { |i| i.is_a? WorkItem }
+        raise "Bad list items: #{bad.inspect}"
+      end
+
       @instructions = instructions
     end
 

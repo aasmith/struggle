906c73a8f65774e5ebe706d48284cf15e701e180
Andrew A Smith <andy@tinnedfruit.org>
2014-01-14 02:06:17 -0800

    Add Hands#remove and Hands#clear for removing a card and clearing out a hand.

--- lib/struggle/hands.rb
+++ lib/struggle/hands.rb
@@ -8,8 +8,18 @@ class Hands
     @hands.fetch(player) << card
   end
 
+  def remove(player, card)
+    @hands.fetch(player).delete(card)
+  end
+
   def get(player)
     @hands.fetch(player).dup.freeze
+  end
+
+  alias hand get
+
+  def clear(player)
+    @hands.fetch(player).clear
   end
 end
 

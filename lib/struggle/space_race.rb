1623621d1bf549915e4d81208fbd31b08372a422
Andrew A Smith <andy@tinnedfruit.org>
2014-02-26 16:19:36 -0800

    Introduce a basic SpaceRace class for tracking the space race.

--- lib/struggle/space_race.rb
+++ lib/struggle/space_race.rb
@@ -1,7 +1,55 @@
+# The space race track stores two pieces of information:
+#
+#  * How many attempts a player has had (reset each turn)
+#  * Where each player is on the track
+#
 class SpaceRace
-  def advance(player)
+
+  LABELS = {
+    0 => "Start",
+    1 => "Earth Satellite",
+    2 => "Animal in Space",
+    3 => "Man in Space",
+    4 => "Man in Earth Orbit",
+    5 => "Lunar Orbit",
+    6 => "Eagle/Bear has Landed",
+    7 => "Space Shuttle",
+    8 => "Space Station",
+  }
+
+  MAX_POSITION = 8
+
+  def initialize
+    @positions = { US => 0, USSR => 0 }
+
+    reset_attempts
   end
 
-  def first?(player)
+  def reset_attempts
+    @num_attempts = { US => 0, USSR => 0 }
   end
+
+  def attempt(superpower)
+    @num_attempts[superpower] += 1
+  end
+
+  def attempts(superpower)
+    @num_attempts.fetch superpower
+  end
+
+  def advance(superpower)
+    return if position(superpower) == MAX_POSITION
+
+    @positions[superpower] += 1
+  end
+
+  def position(superpower)
+    @positions.fetch superpower
+  end
+
+  def label(position)
+    LABELS.fetch position
+  end
+
 end
+
---
+++ test/test_space_race.rb
@@ -0,0 +1,60 @@
+require "helper"
+
+class SpaceRaceTest < Struggle::Test
+
+  def setup
+    @race = SpaceRace.new
+
+    @equal_race = SpaceRace.new
+    @equal_race.attempt(US)
+    @equal_race.advance(US)
+    @equal_race.attempt(USSR)
+    @equal_race.advance(USSR)
+
+  end
+
+  def test_space_race_starts_at_zero
+    assert_equal 0, @race.attempts(US)
+    assert_equal 0, @race.attempts(USSR)
+
+    assert_equal 0, @race.position(US)
+    assert_equal 0, @race.position(USSR)
+  end
+
+  def test_reset_attempts
+    @equal_race.reset_attempts
+
+    assert_equal 0, @equal_race.attempts(US), "Attempts should reset"
+    assert_equal 0, @equal_race.attempts(USSR), "Attempts should reset"
+
+    assert_equal 1, @equal_race.position(US), "Position should not change"
+    assert_equal 1, @equal_race.position(USSR), "Position should not change"
+  end
+
+  def test_attempt
+    @race.attempt(US)
+
+    assert_equal 1, @race.attempts(US)
+    assert_equal 0, @race.attempts(USSR)
+
+    assert_equal 0, @race.position(US), "Attempts do not increase position"
+    assert_equal 0, @race.position(USSR), "Attempts do not increase position"
+  end
+
+  def test_advance
+    @race.advance(US)
+
+    assert_equal 0, @race.attempts(US), "Advances do not increase attempts"
+    assert_equal 0, @race.attempts(USSR), "Advances do not increase attempts"
+
+    assert_equal 1, @race.position(US)
+    assert_equal 0, @race.position(USSR)
+  end
+
+  def test_advance_caps_at_max
+    10.times { @race.advance(US) }
+
+    assert_equal SpaceRace::MAX_POSITION, @race.position(US)
+  end
+
+end

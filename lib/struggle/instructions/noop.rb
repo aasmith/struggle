2aba9e707e7285a2793fade04ac6e63412c967a5
Andrew A Smith <andy@tinnedfruit.org>
2014-02-28 17:41:44 -0800

    Add label to Noop instruction.

--- lib/struggle/instructions/noop.rb
+++ lib/struggle/instructions/noop.rb
@@ -4,7 +4,18 @@
 #
 module Instructions
   class Noop < Instruction
+
+    fancy_accessor :label
+
+    def initialize(label: nil)
+      super
+
+      self.label = label
+    end
+
     def action
+      log label if label
     end
   end
 end
+

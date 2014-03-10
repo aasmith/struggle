c4b26694dbcb412f81d222c5bd14a77fdd7e58be
Andrew A Smith <andy@tinnedfruit.org>
2014-03-09 22:20:42 -0700

    Add Alliance for Progress event

--- lib/struggle/events/card_events/alliance_for_progress.rb
+++ lib/struggle/events/card_events/alliance_for_progress.rb
@@ -8,10 +8,24 @@ module Events
       def action
         instructions = []
 
-        instructions << Instructions::Noop.new(label: "something")
-        instructions << Instructions::Noop.new(label: "dump the card")
+        instructions << Instructions::AwardVictoryPoints.new(
+          player: US,
+          amount: us_controlled_battlegrounds_in_americas
+        )
+
+        instructions << Instructions::Remove.new(
+          card_ref: "AllianceForProgress"
+        )
 
         instructions
+      end
+
+      def us_controlled_battlegrounds_in_americas
+        countries.
+          select { |c| c.in?(CentralAmerica) || c.in?(SouthAmerica) }.
+          select { |c| c.battleground? }.
+          select { |c| c.controlled_by?(US) }.
+          size
       end
 
     end

891408bc2a5e902905f803417d9bc58bce642fdf
Andrew A Smith <andy@tinnedfruit.org>
2014-03-09 19:15:43 -0700

    Add Nasser event.

--- lib/struggle/events/card_events/nasser.rb
+++ lib/struggle/events/card_events/nasser.rb
@@ -8,10 +8,27 @@ module Events
       def action
         instructions = []
 
-        instructions << Instructions::Noop.new(label: "something")
-        instructions << Instructions::Noop.new(label: "dump the card")
+        egypt = countries.find(:egypt)
+
+        instructions << Instructions::AddInfluence.new(
+          influence: USSR,
+          country_name: "Egypt",
+          amount: 2
+        )
+
+        instructions << Instructions::RemoveInfluence.new(
+          influence: US,
+          country_name: "Egypt",
+          amount: half(egypt.influence(US))
+        )
+
+        instructions << Instructions::Remove.new(card_ref: "Nasser")
 
         instructions
+      end
+
+      def half(n)
+        (n / 2.0).ceil
       end
 
     end

88b40898ddaaefba63bdd9765697725e04dae2b4
Andrew A Smith <andy@tinnedfruit.org>
2014-03-11 14:40:04 -0700

    Add TurnMarker#{early,mid,late}_phase? methods

--- lib/struggle/turn_marker.rb
+++ lib/struggle/turn_marker.rb
@@ -11,4 +11,16 @@ class TurnMarker
   def number
     @turn
   end
+
+  def early_phase?
+    (1..3).include? @turn
+  end
+
+  def mid_phase?
+    (4..7).include? @turn
+  end
+
+  def late_phase?
+    (8..10).include? @turn
+  end
 end
--- test/test_turn_marker.rb
+++ test/test_turn_marker.rb
@@ -9,4 +9,33 @@ class TurnMarkerTest < Struggle::Test
     turn.advance
     assert_equal 2, turn.number
   end
+
+  def test_phases
+    turn = TurnMarker.new
+
+    (1..3).each do |n|
+      assert turn.early_phase?, "Turn #{n} should be early phase"
+      refute turn.mid_phase?
+      refute turn.late_phase?
+
+      turn.advance
+    end
+
+    (4..7).each do |n|
+      refute turn.early_phase?
+      assert turn.mid_phase?, "Turn #{n} should be mid phase"
+      refute turn.late_phase?
+
+      turn.advance
+    end
+
+    (8..10).each do |n|
+      refute turn.early_phase?
+      refute turn.mid_phase?
+      assert turn.late_phase?, "Turn #{n} should be late phase"
+
+      turn.advance
+    end
+  end
+
 end

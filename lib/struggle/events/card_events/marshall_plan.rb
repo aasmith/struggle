14286296f2454ba95417a190ba6d855d39074ba9
Andrew A Smith <andy@tinnedfruit.org>
2014-03-11 23:56:24 -0700

    Add Marshall Plan event

--- lib/struggle/events/card_events/marshall_plan.rb
+++ lib/struggle/events/card_events/marshall_plan.rb
@@ -8,10 +8,26 @@ module Events
       def action
         instructions = []
 
-        instructions << Instructions::Noop.new(label: "something")
-        instructions << Instructions::Noop.new(label: "dump the card")
+        instructions << Arbitrators::AddInfluence.new(
+          player: US,
+          influence: US,
+          country_names: non_ussr_controlled_western_europe,
+          limit_per_country: 1,
+          total_influence: 7
+        )
+
+        instructions << Instructions::Remove.new(
+          card_ref: "MarshallPlan"
+        )
 
         instructions
+      end
+
+      def non_ussr_controlled_western_europe
+        countries.
+          select { |c| c.in?(WesternEurope) }.
+          reject { |c| c.controlled_by?(USSR) }.
+          map    { |c| c.name }
       end
 
     end

fe5932d132723410ef7adeedc83dcb5a1444d963
Andrew A Smith <andy@tinnedfruit.org>
2014-03-11 23:50:26 -0700

    Add Pershing II Deployed event

--- lib/struggle/events/card_events.rb
+++ lib/struggle/events/card_events.rb
@@ -73,7 +73,7 @@ require_relative "card_events/one_small_step"
 require_relative "card_events/opec"                             # TODO
 require_relative "card_events/ortega_elected_in_nicaragua"      # TODO
 require_relative "card_events/panama_canal_returned"
-require_relative "card_events/pershing_ii_deployed"             # TODO
+require_relative "card_events/pershing_ii_deployed"
 require_relative "card_events/portuguese_empire_crumbles"
 require_relative "card_events/puppet_governments"               # TODO
 require_relative "card_events/quagmire"                         # TODO
--- lib/struggle/events/card_events/pershing_ii_deployed.rb
+++ lib/struggle/events/card_events/pershing_ii_deployed.rb
@@ -8,10 +8,30 @@ module Events
       def action
         instructions = []
 
-        instructions << Instructions::Noop.new(label: "something")
-        instructions << Instructions::Noop.new(label: "dump the card")
+        instructions << Instructions::AwardVictoryPoints.new(
+          player: USSR,
+          amount: 1
+        )
+
+        instructions << Arbitrators::RemoveInfluence.new(
+                     player: USSR,
+                  influence: US,
+              country_names: western_europe,
+          limit_per_country: 1,
+            total_influence: 3
+        )
+
+        instructions << Instructions::Remove.new(
+          card_ref: "PershingIiDeployed"
+        )
 
         instructions
+      end
+
+      def western_europe
+        countries.
+          select { |c| c.in?(WesternEurope) }.
+          map    { |c| c.name }
       end
 
     end

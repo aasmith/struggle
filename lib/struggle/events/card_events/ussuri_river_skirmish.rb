f6db61819481256e4921981434e244f20f2dd092
Andrew A Smith <andy@tinnedfruit.org>
2014-03-12 00:04:15 -0700

    Add Ussuri River Skirmish event

--- lib/struggle/events/card_events/ussuri_river_skirmish.rb
+++ lib/struggle/events/card_events/ussuri_river_skirmish.rb
@@ -3,15 +3,37 @@ module Events
 
     class UssuriRiverSkirmish < Instruction
 
-      needs :countries
+      needs :countries, :china_card
 
       def action
         instructions = []
 
-        instructions << Instructions::Noop.new(label: "something")
-        instructions << Instructions::Noop.new(label: "dump the card")
+        if china_card.holder.ussr?
+          instructions << Instructions::ClaimChinaCard.new(
+            player: US,
+            playable: true
+          )
+        else
+          instructions << Arbitrators::AddInfluence.new(
+                       player: US,
+                    influence: US,
+                country_names: asia,
+            limit_per_country: 2,
+              total_influence: 4
+          )
+        end
+
+        instructions << Instructions::Remove.new(
+          card_ref: "UssuriRiverSkirmish"
+        )
 
         instructions
+      end
+
+      def asia
+        countries.
+          select { |c| c.in?(Asia) }.
+          map    { |c| c.name }
       end
 
     end

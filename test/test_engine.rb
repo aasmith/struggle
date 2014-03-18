01def41ec240721a8e1a58e58732142a606ab569
Andrew A Smith <andy@tinnedfruit.org>
2014-03-17 19:07:20 -0700

    Move NestingInstruction into a testing role.

--- test/test_engine.rb
+++ test/test_engine.rb
@@ -365,5 +365,22 @@ class TestEngine < Struggle::Test
       @block.call
     end
   end
+
+  class NestingInstruction < Instruction
+    def initialize(*instructions)
+      super()
+
+      unless instructions.all? { |i| WorkItem === i }
+        bad = instructions.reject { |i| i.is_a? WorkItem }
+        raise "Bad list items: #{bad.inspect}"
+      end
+
+      @instructions = instructions
+    end
+
+    def action
+      @instructions
+    end
+  end
 end
 

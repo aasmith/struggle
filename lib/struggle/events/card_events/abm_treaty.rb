f687b8b70e792d10177d91498dc6dd2467020b87
Andrew A Smith <andy@tinnedfruit.org>
2014-12-18 21:11:50 -0800

    Add ABM Treaty

--- lib/struggle/events/card_events/abm_treaty.rb
+++ lib/struggle/events/card_events/abm_treaty.rb
@@ -3,13 +3,21 @@ module Events
 
     class AbmTreaty < Instruction
 
-      needs :countries
+      needs :phasing_player
 
       def action
         instructions = []
 
-        instructions << Instructions::Noop.new(label: "something")
-        instructions << Instructions::Noop.new(label: "dump the card")
+        instructions << Instructions::ImproveDefcon.new(amount: 1)
+
+        instructions << Arbitrators::FreeMove.new(
+          player: phasing_player,
+             ops: 4
+        )
+
+        instructions << Instructions::Discard.new(
+          card_ref: "AbmTreaty"
+        )
 
         instructions
       end

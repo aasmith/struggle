507aeef05c3091daaf3dd46fb84bec348d4f721c
Andrew A Smith <andy@tinnedfruit.org>
2014-12-28 14:27:02 -0800

    Add 'Junta' event.

--- lib/struggle/events/card_events.rb
+++ lib/struggle/events/card_events.rb
@@ -49,7 +49,7 @@ require_relative "card_events/iran_contra_scandal"              # TODO
 require_relative "card_events/iran_iraq_war"
 require_relative "card_events/iranian_hostage_crisis"
 require_relative "card_events/john_paul_ii_elected_pope"
-require_relative "card_events/junta"                            # TODO
+require_relative "card_events/junta"
 require_relative "card_events/kitchen_debates"
 require_relative "card_events/korean_war"
 require_relative "card_events/latin_american_death_squads"      # TODO
--- lib/struggle/events/card_events/junta.rb
+++ lib/struggle/events/card_events/junta.rb
@@ -3,15 +3,48 @@ module Events
 
     class Junta < Instruction
 
-      needs :countries
+      needs :countries, :observers, :cards, :phasing_player
 
       def action
         instructions = []
 
-        instructions << Instructions::Noop.new(label: "something")
-        instructions << Instructions::Noop.new(label: "dump the card")
+        instructions << Arbitrators::AddInfluence.new(
+                   player: phasing_player,
+                influence: phasing_player,
+            country_names: south_or_central_america.map(&:name),
+          total_influence: 2,
+          total_countries: 1
+        )
+
+        card = cards.find_by_ref("Junta")
+        mods = observers.ops_modifiers_for_player(phasing_player)
+
+        free_coup = Arbitrators::FreeCoup.new(
+                 player: phasing_player,
+            ops_counter: card.ops_counter(mods),
+          country_names: south_or_central_america.map(&:name)
+        )
+
+        realignment = Arbitrators::Realignment.new(
+                 player: phasing_player,
+            ops_counter: card.ops_counter(mods),
+          country_names: south_or_central_america.map(&:name)
+        )
+
+        instructions << Arbitrators::Proxy.new(
+           player: phasing_player,
+          choices: [free_coup, realignment]
+        )
+
+        instructions << Instructions::Discard.new(
+          card_ref: "Junta"
+        )
 
         instructions
+      end
+
+      def south_or_central_america
+        countries.select { |c| c.in?(CentralAmerica) || c.in?(SouthAmerica) }
       end
 
     end

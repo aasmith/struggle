3063517d45011b50b0ee654beb225a4c1f218ccd
Andrew A Smith <andy@tinnedfruit.org>
2016-11-25 11:59:50 -0800

    Don't over-allocate USSR influence in Romania.
    Only add enough USSR influence for control of Romania when playing the
    Romanian Abdication event. If the USSR already had some influence in
    Romania, too much would be added as a result of this event.

--- lib/struggle/events/card_events/romanian_abdication.rb
+++ lib/struggle/events/card_events/romanian_abdication.rb
@@ -16,11 +16,20 @@ module Events
           amount: romania.influence(US)
         )
 
-        instructions << Instructions::AddInfluence.new(
-          influence: USSR,
-          country_name: "Romania",
-          amount: romania.stability
-        )
+        ussr_influence_for_control = romania.stability - romania.influence(USSR)
+
+        if ussr_influence_for_control > 0
+
+          log "USSR needs %s more points for control of Romania." % [
+            ussr_influence_for_control
+          ]
+
+          instructions << Instructions::AddInfluence.new(
+            influence: USSR,
+            country_name: "Romania",
+            amount: ussr_influence_for_control
+          )
+        end
 
         instructions << Instructions::Remove.new(
           card_ref: "RomanianAbdication"
